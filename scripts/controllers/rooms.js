7099df6a5353aa2c6e2c940e4d2cda518d549a76
rezraf21 <rezraf21@yahoo.com>
2015-04-07 21:12:26 -0700

    rooms.js

--- scripts/controllers/rooms.js
+++ scripts/controllers/rooms.js
@@ -9,5 +9,15 @@
     var myParagraph = document.createElement('p');
     myParagraph.innerHTML = 'This is a paragraph that is generated from the roomsCtrl!';
     this.mainView.appendChild(myParagraph);
+    var select = document.querySelector('form > .pa-instructors-select');
+
+    PA.getJSON('json/rooms.json', function (data) {
+      var rooms = data.rooms;
+      for (var i = 0; i < rooms.length; i++) {
+        var option = document.createElement('option');
+        option.innerHTML = rooms[i].roomNo;
+        select.appendChild(option);
+      }
+    });
   };
 })();
